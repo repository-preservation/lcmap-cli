b5b47c642ad61c9e5f6042579095e409dfb2d6df
davidvhill <david.hill.ctr@usgs.gov>
2018-12-17 14:02:35 -0600

    add config file requirement + update readme

--- README.md
+++ README.md
@@ -53,6 +53,58 @@ Not all commands accept all parameters.  Use lcmap <command> -h for usage
 |  --verbose  | display additional information                    |
 | -h --help   | display help                                      |
 
+## Configuration
+lcmap-cli requires an config file at ~/.usgs/lcmap-cli.edn.
+
+```edn
+
+{:http  {:timeout 2400000}
+ :grids {:conus  {:ard "http://host:port/ard_cu_c01_v01"
+                  :aux "http://host:port/aux_cu_c01_v01"
+                  :ccdc "http://host:port/ard_cu_c01_v01_aux_cu_v01_ccdc_1_0"
+                  :grid "/grid"
+                  :snap "/grid/snap"
+                  :near "/grid/near"
+                  :inventory "/inventory"
+                  :sources "/sources"
+                  :tile "/tile"
+                  :chip "/chip"
+                  :pixel "/pixel"
+                  :segment "/segment"
+                  :annual-prediction "/annual_prediction"
+                  :segment-instance-count 25
+                  :segment-sleep-for 1000}
+         :alaska {:ard "http://host:port/ard_ak_c01_v01"
+                  :aux "http://host:port/aux_ak_v01"
+                  :ccdc "http://host:port/ard_ak_c01_v01_aux_ak_v01_ccdc_1_0"
+                  :grid "/grid"
+                  :snap "/grid/snap"
+                  :near "/grid/near"
+                  :inventory "/inventory"
+                  :sources "/sources"
+                  :tile "/tile"
+                  :chip "/chip"
+                  :pixel "/pixel"
+                  :segment "/segment"
+                  :annual-prediction "/annual_prediction"
+                  :segment-instance-count 1
+                  :segment-sleep-for 1000}
+         :hawaii {:ard "http://host:port/ard_hi_c01_v01"
+                  :aux "http://host:port/aux_hi_v01"
+                  :ccdc "http://host:port/ard_hi_c01_v01_aux_hi_v01_ccdc_1_0"
+                  :grid "/grid"
+                  :snap "/grid/snap"
+                  :near "/grid/near"
+                  :inventory "/inventory"
+                  :sources "/sources"
+                  :tile "/tile"
+                  :chip "/chip"
+                  :pixel "/pixel"
+                  :segment "/segment"
+                  :annual-prediction "/annual_prediction"
+                  :segment-instance-count 1
+                  :segment-sleep-for 1000}}}
+```
 
 ## Examples
 
--- src/lcmap_cli/config.clj
+++ src/lcmap_cli/config.clj
@@ -1,68 +1,30 @@
-(ns lcmap-cli.config)
+(ns lcmap-cli.config
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import java.io.PushbackReader))
+
+(def edn-file (str (System/getProperty "user.home") "/.usgs/lcmap-cli.edn"))
+
+(defn load-edn
+  [source]
+  (try
+    (with-open [r (io/reader source)]
+      (edn/read (java.io.PushbackReader. r)))
+    (catch java.io.IOException e
+      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
+    (catch RuntimeException e
+      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e))))) 
+
+(defn grids
+  []
+  (if-let [c (load-edn edn-file)]
+    (:grids c)
+    nil))
+
+(defn http-options
+  []
+  (if-let [c (load-edn edn-file)]
+    (:http c)))
 
 
-;;  (:require[clojure.edn :require [read]]
-;;           [clojure.io :as io)
-;;  (:import java.io.PushbackReader)))
-
-;;(defn load-edn
-;;  "Load edn from an io/reader source (filename or io/resource)."
-;;  [source]
-;;  (try
-;;    (with-open [r (io/reader source)]
-;;      (edn/read (java.io.PushbackReader. r)))
-
-;;    (catch java.io.IOException e
-;;      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
-;;    (catch RuntimeException e
-;;      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
-
-(def http-options
-  {:timeout 2400000})
-  
-(def grids
-  {:conus {:ard "http://lcmap-test.cr.usgs.gov/ard_cu_c01_v01"
-           :aux "http://lcmap-test.cr.usgs.gov/aux_cu_c01_v01"
-           :ccdc "http://lcmap-test.cr.usgs.gov/ard_cu_c01_v01_aux_cu_v01_ccdc_1_0"
-           :grid "/grid"
-           :snap "/grid/snap"
-           :near "/grid/near"
-           :inventory "/inventory"
-           :sources "/sources"
-           :tile "/tile"
-           :chip "/chip"
-           :pixel "/pixel"
-           :segment "/segment"
-           :annual-prediction "/annual_prediction"
-           :segment-instance-count 25
-           :segment-sleep-for 1000}
-   :alaska {:ard "http://host:port/ard_ak_c01_v01"
-            :aux "http://host:port/aux_ak_v01"
-            :ccdc "http://host:port/ard_ak_c01_v01_aux_ak_v01_ccdc_1_0"
-            :grid "/grid"
-            :snap "/grid/snap"
-            :near "/grid/near"
-            :inventory "/inventory"
-            :sources "/sources"
-            :tile "/tile"
-            :chip "/chip"
-            :pixel "/pixel"
-            :segment "/segment"
-            :annual-prediction "/annual_prediction"
-            :segment-instance-count 1
-            :segment-sleep-for 1000}
-   :hawaii {:ard "http://host:port/ard_hi_c01_v01"
-            :aux "http://host:port/aux_hi_v01"
-            :ccdc "http://host:port/ard_hi_c01_v01_aux_hi_v01_ccdc_1_0"
-            :grid "/grid"
-            :snap "/grid/snap"
-            :near "/grid/near"
-            :inventory "/inventory"
-            :sources "/sources"
-            :tile "/tile"
-            :chip "/chip"
-            :pixel "/pixel"
-            :segment "/segment"
-            :annual-prediction "/annual_prediction"
-            :segment-instance-count 1
-            :segment-sleep-for 1000}})
