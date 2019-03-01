ee1cde0de3d31cf7d089af96effd0113003dce5b
David Hill <davehill75@gmail.com>
2019-03-01 14:23:22 -0600

    lcmap-cli.functions-test/to-json-or-str-test passes

--- test/lcmap_cli/functions_test.clj
+++ test/lcmap_cli/functions_test.clj
@@ -37,12 +37,46 @@
   
   (testing "(to-json Exception)"
     (is (thrown? com.fasterxml.jackson.core.JsonGenerationException
-                 (to-json (new java.lang.Object)))))
+                 (to-json (new java.lang.Object))))))
 
 
 (deftest to-json-or-str-test
+
+  (testing "(to-json-or-str Hashmap)"
+    (is (= "{\"key\":\"value\"}" (to-json-or-str {:key "value"}))))
+
+  (testing "(to-json-or-str Integer)"
+    (is (= "1" (to-json-or-str 1))))
+
+  (testing "(to-json-or-str Float)"
+    (is (= "1.0" (to-json-or-str 1.0))))
+
+  (testing "(to-json-or-str Boolean)"
+    (is (= "true" (to-json-or-str true))))
+
+  (testing "(to-json-or-str Vector)"
+    (is (= "[1,2,3]" (to-json-or-str [1 2 3]))))
+
+  (testing "(to-json-or-str List)"
+    (is (= "[1,2,3]" (to-json-or-str '(1 2 3)))))
+
+  (testing "(to-json-or-str Set)"
+    (is (= "[1,3,2]" (to-json-or-str #{1 2 3}))))
+  
+  (testing "(to-json-or-str String)"
+    (is (= "\"a-value\"" (to-json-or-str "a-value"))))
+
+  (testing "(to-json-or-str Keyword)"
+    (is (= "\"a-keyword\"" (to-json-or-str :a-keyword))))
+
+  (testing "(to-json-or-str Rational)"
+    (is (= "0.3333333333333333" (to-json-or-str (/ 1 3)))))
+  
+  (testing "(to-json-or-str Exception)"
+    (is (not (nil? (to-json-or-str (new java.lang.Object))))))
+        
   (testing "to-json-or-str-test-passes"
-    (= 1 0)))
+    (= 1 0))
 
   (testing "to-json-or-str-test-"
     (= 1 0)))
