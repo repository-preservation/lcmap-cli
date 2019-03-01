151debd9f39f35828ca9ebac7d7b2568c41c4ef6
David Hill <davehill75@gmail.com>
2019-03-01 14:06:33 -0600

    lcmap-cli.functions-test/to-json-test passes

--- test/lcmap_cli/functions_test.clj
+++ test/lcmap_cli/functions_test.clj
@@ -2,7 +2,49 @@
   (:require [clojure.test :refer :all]
             [lcmap-cli.functions :refer :all]))
 
+
 (deftest to-json-test
-  (testing "to-json pass"
-    (is (= "{\"key\":\"value\"}" (to-json {:key "value"})))))
+
+  (testing "(to-json Hashmap)"
+    (is (= "{\"key\":\"value\"}" (to-json {:key "value"}))))
+
+  (testing "(to-json Integer)"
+    (is (= "1" (to-json 1))))
+
+  (testing "(to-json Float)"
+    (is (= "1.0" (to-json 1.0))))
+
+  (testing "(to-json Boolean)"
+    (is (= "true" (to-json true))))
+
+  (testing "(to-json Vector)"
+    (is (= "[1,2,3]" (to-json [1 2 3]))))
+
+  (testing "(to-json List)"
+    (is (= "[1,2,3]" (to-json '(1 2 3)))))
+
+  (testing "(to-json Set)"
+    (is (= "[1,3,2]" (to-json #{1 2 3}))))
+  
+  (testing "(to-json String)"
+    (is (= "\"a-value\"" (to-json "a-value"))))
+
+  (testing "(to-json Keyword)"
+    (is (= "\"a-keyword\"" (to-json :a-keyword))))
+
+  (testing "(to-json Rational)"
+    (is (= "0.3333333333333333" (to-json (/ 1 3)))))
+  
+  (testing "(to-json Exception)"
+    (is (thrown? Exception (to-json (new java.lang.Object)))))
+
+
+(deftest to-json-or-str-test
+  (testing "to-json-or-str-test-passes"
+    (= 1 0)))
+
+  (testing "to-json-or-str-test-"
+    (= 1 0)))
+
+
 
