40cf78716349f1cb3a2aeb01faa13a6814f1d6f7
David Hill <davehill75@gmail.com>
2019-03-04 15:21:07 -0600

    config is tested.  all tests pass

--- test/lcmap_cli/config_test.clj
+++ test/lcmap_cli/config_test.clj
@@ -9,3 +9,13 @@
     (is (= (type (load-edn edn-file)) clojure.lang.PersistentArrayMap))
     (is (not (nil? (:http (load-edn edn-file)))))
     (is (not (nil? (:grids (load-edn edn-file)))))))
+
+
+(deftest grids-test
+  (testing "grids from edn"
+    (is (not (nil? (:fake-http grids))))))
+
+
+(deftest http-options-test
+  (testing "http-options from edn"
+    (is (not (nil? (:timeout http-options))))))
