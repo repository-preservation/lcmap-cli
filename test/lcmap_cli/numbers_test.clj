6f5c775dfe48b981074717fc048679ae1fc32a98
David Hill <davehill75@gmail.com>
2019-03-05 12:09:39 -0600

    lcmap-cli.numbers_test is tested

--- test/lcmap_cli/numbers_test.clj
+++ test/lcmap_cli/numbers_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [lcmap-cli.numbers :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest numerize-test
+  (testing "(numberize)"
+    (is (nil? (numberize true)))
+    (is (= 0 (numberize 0)))
+    (is (= 0 (numberize "0")))
+    (is (= 0.0 (numberize "0.0")))
+    (is (= 0.0 (numberize 0.0)))
+    (is (= -10.25 (numberize "-10.25")))
+    (is (= 1234 (numberize "1234asdf")))
+    (is (nil? (numberize "asdf1234")))))
