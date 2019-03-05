369206bd0bcfe09f3111c9364ef2e52ba18ab5cc
David Hill <davehill75@gmail.com>
2019-03-05 12:20:39 -0600

    url-test and http-options test passes

--- test/lcmap_cli/http_test.clj
+++ test/lcmap_cli/http_test.clj
@@ -1,7 +1,33 @@
 (ns lcmap-cli.http-test
   (:require [clojure.test :refer :all]
-            [lcmap-cli.http :refer :all]))
+            [lcmap-cli.http :refer :all]
+            [org.httpkit.fake :refer [with-fake-http]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest url-test
+  (testing "(url)"
+    (is (= (url :fake-http :ard :snap)
+         "http://fake/grid/snap"))))
+
+(deftest http-options-test
+  (testing "(http-options)"
+    (is (= (http-options {:keepalive 30000})
+           {:keepalive 30000 :timeout 2400000}))))
+      
+  (comment (testing "(url)"
+       (with-fake-http ["http://fake/grid" "[{\"name\": \"tile\",
+                                           \"proj\": \"\",
+                                           \"rx\": 1.0,
+                                           \"ry\": -1.0,
+                                           \"sx\": 150000.0,
+                                           \"sy\": 150000.0,
+                                           \"tx\": 2565585.0,
+                                           \"ty\": 3314805.0},
+                                          {\"name\": \"chip\",
+                                           \"proj\": \"\",
+                                           \"rx\": 1.0,
+                                           \"ry\": -1.0,
+                                           \"sx\": 3000.0,
+                                           \"sy\": 3000.0,
+                                           \"tx\": 2565585.0,
+                                           \"ty\": 3314805.0}]]"])))
