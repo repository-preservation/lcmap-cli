61ee1d80a80bcbd248edc094ac961bf6b88a1750
David Hill <davehill75@gmail.com>
2019-03-05 14:51:42 -0600

    usage, options and error-msg tests pass

--- test/lcmap_cli/core_test.clj
+++ test/lcmap_cli/core_test.clj
@@ -2,6 +2,45 @@
   (:require [clojure.test :refer :all]
             [lcmap-cli.core :refer :all]))
 
-(deftest a-test
+
+(deftest options-test
+  (testing "(options)"
+    (is (= '(["-h" "--help"]) (options [:help])))))
+
+
+(deftest registry-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+
+(deftest usage-test
+  (testing "(usage)"
+    (= java.lang.String
+       (type (usage "some-action" "some-options")))))
+
+(deftest actions-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+
+(deftest error-msg-test
+  (testing "(error-msg)"
+    (= java.lang.String
+       (type (error-msg ["one" "two" "three"])))))
+
+
+(deftest function-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+
+(deftest parameters-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+
+(deftest validate-args-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+
