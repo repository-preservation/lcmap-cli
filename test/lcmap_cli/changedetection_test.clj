d6a515eff696e93edb64379d2a19bd5d27a65a9d
David Hill <davehill75@gmail.com>
2019-03-05 13:50:30 -0600

    lcmap-cli.changedetection_test/handler-test passes

--- test/lcmap_cli/changedetection_test.clj
+++ test/lcmap_cli/changedetection_test.clj
@@ -2,6 +2,38 @@
   (:require [clojure.test :refer :all]
             [lcmap-cli.changedetection :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest handler-test
+  (testing "(handler)"
+    (is (= (handler {:cx 0
+                     :cy 0
+                     :grid "fake-http"
+                     :acquired "1980/2019"
+                     :response (atom {:status 200
+                                      :headers {:content-type "application/json"}
+                                      :body "[\"some-value\"]"})})
+           ["some-value"]))
+    
+    (is (= (handler {:cx 0
+                     :cy 0
+                     :grid "fake-http"
+                     :acquired "1980/2019"
+                     :response (atom {:status 500
+                                      :headers {:content-type "application/json"}
+                                      :body "[\"some-value\"]"})})
+           {:cx 0 :cy 0 :acquired "1980/2019" :error {:response {:status 500
+                                                                 :headers {:content-type "application/json"}
+                                                                 :body "[\"some-value\"]"}}}))
+    
+    (every? #{:cx :cy :grid :acquired :error}
+            (keys (handler {:cx 0
+                            :cy 0
+                            :grid "fake-http"
+                            :acquired "1980/2019"
+                            :response {:status 200
+                                       :headers {:content-type "application/json"}
+                                       :body "some-value\"]"}})))))
+       
+
+    
+
+
