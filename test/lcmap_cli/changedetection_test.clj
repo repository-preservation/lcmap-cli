7e58098c499cf70339fb9b0c2e73011c6c9a1673
David Hill <davehill75@gmail.com>
2019-03-05 13:55:48 -0600

    lcmap-cli.changedetection_test/handler-test passes

--- test/lcmap_cli/changedetection_test.clj
+++ test/lcmap_cli/changedetection_test.clj
@@ -3,7 +3,7 @@
             [lcmap-cli.changedetection :refer :all]))
 
 (deftest handler-test
-  (testing "(handler)"
+  (testing "(handler) with HTTP 200"
     (is (= (handler {:cx 0
                      :cy 0
                      :grid "fake-http"
@@ -11,8 +11,8 @@
                      :response (atom {:status 200
                                       :headers {:content-type "application/json"}
                                       :body "[\"some-value\"]"})})
-           ["some-value"]))
-    
+           ["some-value"])))
+  (testing "(handler) with HTTP 500"
     (is (= (handler {:cx 0
                      :cy 0
                      :grid "fake-http"
@@ -22,8 +22,9 @@
                                       :body "[\"some-value\"]"})})
            {:cx 0 :cy 0 :acquired "1980/2019" :error {:response {:status 500
                                                                  :headers {:content-type "application/json"}
-                                                                 :body "[\"some-value\"]"}}}))
-    
+                                                                 :body "[\"some-value\"]"}}})))
+
+  (testing "(handler) with HTTP 200 but decode failure"
     (every? #{:cx :cy :grid :acquired :error}
             (keys (handler {:cx 0
                             :cy 0
