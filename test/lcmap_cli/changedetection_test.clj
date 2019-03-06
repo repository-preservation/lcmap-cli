9bcabd46a51f470a8c2238061c53f1cb9fa3638e
David Hill <davehill75@gmail.com>
2019-03-06 16:16:06 -0600

    corrected change detection test

--- test/lcmap_cli/changedetection_test.clj
+++ test/lcmap_cli/changedetection_test.clj
@@ -15,6 +15,7 @@
                                       :headers {:content-type "application/json"}
                                       :body "[\"some-value\"]"})})
            ["some-value"])))
+  
   (testing "(handler) with HTTP 500"
     (is (= (handler {:cx 0
                      :cy 0
@@ -23,9 +24,10 @@
                      :response (atom {:status 500
                                       :headers {:content-type "application/json"}
                                       :body "[\"some-value\"]"})})
-           {:cx 0 :cy 0 :acquired "1980/2019" :error {:response {:status 500
-                                                                 :headers {:content-type "application/json"}
-                                                                 :body "[\"some-value\"]"}}})))
+           {:cx 0
+            :cy 0
+            :acquired "1980/2019"
+            :error "{:response {:status 500, :headers {:content-type \"application/json\"}, :body \"[\\\"some-value\\\"]\"}}"})))
 
   (testing "(handler) with HTTP 200 but decode failure"
     (every? #{:cx :cy :grid :acquired :error}
