8e5852ed121b7251dadfd148c89cbc8c8bdf4137
clay austin <clay.austin.ctr@usgs.gov>
2019-03-20 13:40:37 -0500

    fix products tests for http options passing

--- test/lcmap_cli/products_test.clj
+++ test/lcmap_cli/products_test.clj
@@ -10,8 +10,6 @@
             [environ.core :as environ]
             [clojure.core.async :as async]))
 
-;(def test_config {:grids {:conus {:ard "http://localardhost:5656" :ccdc "http://localccdchost:5656" :products "/products"}}})
-
 (deftest handler-test
   (testing "handler with 200 status"
     (is (= ["foo"]
@@ -37,20 +35,20 @@
            {:body "{\"grid\":\"foo\",\"resource\":\"bar\"}", :headers {"Content-Type" "application/json"}, :post :foo, :ccdc :bar}))))
 
 (deftest start-consumers-test
-  (with-redefs [products/handler str
-                products/post-request inc]
+  (with-redefs [products/handler      str
+                products/post-request (fn [i] (+ (:val i) (get-in i [:http-options :timeout])))
+                cfg/http-options      {:timeout 9}]
 
     (let [in-chan state/tile-in
           out-chan state/tile-out
           chunk-size 2
           consumers (products/start-consumers chunk-size in-chan out-chan)
-          vals [4]]
+          vals [{:val 4}]]
 
-      (async/go
-        (doseq [i vals]
-          (async/>! in-chan i)))
+      (doseq [i vals]
+        (async/>!! in-chan i))
 
-        (is (= "5" (async/<!! out-chan))))))
+      (is (= "13" (async/<!! out-chan))))))
 
 (deftest date-range-test
   (with-redefs [cfg/product-doy (fn [i] "07-01")]
@@ -60,31 +58,12 @@
 (deftest products-test
   (with-redefs [cfg/request-instance-count (fn [i] 1)
                 functions/chips       (fn [i] [{:cx 1 :cy 2}])
-                functions/tile-to-xy (fn [i] {:x 3 :y 4})
-                cfg/product-doy      (fn [i] "07-01")
-                products/handler str
-                products/post-request keys]
+                functions/tile-to-xy  (fn [i] {:x 3 :y 4})
+                cfg/product-doy       (fn [i] "07-01")
+                products/handler      str
+                products/post-request keys
+                cfg/http-options      {:timeout 9}]
 
-    (is (= '("(:tile :dates :grid :product :cx :resource :cy)")
-           (products/products {:grid "conus" :tile "027008" :product "tsc" :years "2006"}  )))
+    (is (= '("(:tile :dates :grid :product :cx :resource :cy :http-options)")
+       (products/products {:grid "conus" :tile "027008" :product "tsc" :years "2006"})))))
 
-    )
-
-
-
-)
-
-
-
-
-;; (deftest product-request-test
-;;   (with-fake-http [{:url "http://localccdchost:5656/products" :method :post} {:status 200 :body "sweet_geotiff.tif"}]
-;;     (let [response (products/post-request {:grid "conus" :penny "red"})]
-;;       (is (= 200 (:status @response))))))
-
-;; (deftest response-handler-test
-;;   (let [resp (promise)
-;;         delv (deliver resp {:status 200 :body "true" :headers {:content-type "application/json"}}) 
-;;         input {:grid "conus" :date "2006-07-01" :product "primary-landcover" :tile "005007" :tilex -1815585 :tiley 2264805 :response resp }]
-    
-;;     (is (= true (:body (products/response-handler input))))))
