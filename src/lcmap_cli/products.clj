c1e1455e109edfd43cac77fda01c33297708d478
clay austin <clay.austin.ctr@usgs.gov>
2019-04-17 10:21:34 -0500

    add product-chip option

--- src/lcmap_cli/products.clj
+++ src/lcmap_cli/products.clj
@@ -50,6 +50,15 @@
         mmdd (cfg/product-mmdd grid)]
     (map (fn [i] (str i "-" mmdd)) year_range)))
 
+(defn chip
+  [{grid :grid tile :tile product :product years :years cx :cx cy :cy :as all}]
+  (let [date-coll (date-range all)
+        req-args  (-> (dissoc all :years) (assoc :dates date-coll :resource "product" :http-options cfg/http-options)) 
+        response  (post-request req-args)
+        output    (handler response)]
+    (f/output output)
+    output))
+
 (defn product
   [{grid :grid tile :tile product :product years :years :as all}]
   (let [chunk-size (cfg/product-instance-count grid)
